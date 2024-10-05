69a560acb1481c25efb02c66070c5163655cc7bf
HenIsTheMan <2169test2169@gmail.com>
2024-10-05 15:22:36 +0800

    Almost done

--- arrayFunction.asm
+++ arrayFunction.asm
@@ -24,28 +24,43 @@ main:
 	addi $t1, $v0, 0    	# second user input in $t1
 
 	# Call the findMin function
-	# setup the parameter(s)
-	# call the function
 
+	## setup the parameter(s)
+	la $a0, array			# Just in case
+	la $a1, array
+
+	sll $t0, $t0, 2			# $t0 *= 4
+	sll $t1, $t1, 2			# $t1 *= 4
+
+	add $a0, $a0, $t0		# $a0 += $t0 ($a0 = &A[X])
+	add $a1, $a1, $t1		# $a1 += $t1 ($a1 = &A[Y])
+
+	## call the function
+	jal	findMin
 
 	# Print the min item
-	# place the min item in $t3	for printing
 
-	# Print an integer followed by a newline
+	## place the min item in $t3 for printing
+	lw   $t3, 0($v0)	# $t3 is the current item
+
+	## Print an integer followed by a newline
 	li   $v0, 1   		# system call code for print_int
     addi $a0, $t3, 0    # print $t3
     syscall       		# make system call
 
 	li   $v0, 4   		# system call code for print_string
-    la   $a0, newl    	# 
+    la   $a0, newl    	# print "\n"
     syscall       		# print newline
 
-	#Calculate and print the index of min item
+	# Calculate and print the index of min item
 	
-	# Place the min index in $t3 for printing	
+	## Place the min index in $t3 for printing
 
-	# Print the min index
-	# Print an integer followed by a newline
+
+
+	## Print the min index
+
+	### Print an integer followed by a newline
 	li   $v0, 1   		# system call code for print_int
     addi $a0, $t3, 0    # print $t3
     syscall       		# make system call
@@ -88,10 +103,31 @@ e1:
 #######################################################################
 ###   Student Function findMin   ### 
 #Input: Lower Array Pointer in $a0, Higher Array Pointer in $a1
-#Output: $v0 contains the address of min item 
-#Purpose: Find and return the minimum item 
-#              between $a0 and $a1 (inclusive)
-#Registers used: <Fill in with your register usage>
+#Output: $t4 contains the address of min item 
+#Purpose: Find and return the minimum item between $a0 and $a1 (inclusive)
+#Registers used: $t4, $t5, $t6 (shld be can use $t0 - $t3 but just in case)
 #Assumption: Array element is word size (4-byte), $a0 <= $a1
 findMin:
-	jr $ra			# return from this function
+	li $t4, 2147483647		# Load INT_MAX into $t4
+
+LoopStart:
+	slt $t6, $a1, $a0		# $t6 = $a1 < $a0
+
+	bne $t6, $0, LoopEnd	# Goto LoopEnd if $a1 < $a0 ($a0 > $a1)
+
+	lw $t5, 0($a0)			# Load curr element of A into $t5
+
+	slt $t6, $t5, $t4		# $t5 = $t5 < $t4
+
+	beq $t6, $0, LoopUpdate
+
+	addi $t4, $t5, 0		# $t4 = $t5 since $t5 contains new min val
+	addi $v0, $a0, 0		# $v0 = $a0 since $a0 contains address of new min val
+
+LoopUpdate:
+	addi $a0, $a0, 4		# $a0 += 4
+
+	j LoopStart				# LoopBack
+
+LoopEnd:
+	jr $ra					# return from this function
